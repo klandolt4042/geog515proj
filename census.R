45cf7b62b2b7003a0e4e53a0d01027ea3a18d99c
klandolt4042 <klandolt@vols.utk.edu>
2016-10-01 17:30:10 -0400

    added orfeo and harco shapefile

--- census.R
+++ census.R
@@ -2,14 +2,21 @@
 library(acs)
 library(tigris)
 library(stringr)
+library(rgdal)
 
 ***REMOVED***
 
 #Texas is 48
 #Harris County is 201
 
+tx <- counties("Texas", cb=TRUE)
+harris <- tx[tx$COUNTYFP == 201,]
+
 geo <- geo.make(state=c("TX"), county= 201, tract = "*")
+geo1 = geo.make(state="TX", county=201, tract = "*", block.group="*")
 
 counties <- 201
-tracts <- tracts(state="TX", county=201)
+# tracts <- tracts(state="TX", county=201)
+# blocks <- blocks(state="TX", county=201)
+
 income <- acs.fetch(endyear = 2014, span = 5, geography = geo, table.number = "B19001", col.names="pretty")
