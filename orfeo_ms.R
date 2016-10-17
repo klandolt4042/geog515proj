c2c313f630ff00edc945a97d0f604aaba8665b05
klandolt4042 <klandolt@vols.utk.edu>
2016-10-16 23:00:07 -0400

    change

--- orfeo_ms.R
+++ orfeo_ms.R
@@ -11,6 +11,16 @@ NDVI <- function(input, out){
 }
 # NDVI(input = "E://geog515//naip_tiffs//m_2909401_ne_15_1_20141015_20141201.tif", out = "E://geog515//processed//testndvi.tif")
 
+NDVIScale <- function(input, output){
+  if(missing(input)){
+    stop("")}
+  if(missing(output)){
+    stop("")}
+  o_dir <- "E:/OTB-5.6.1-win64/OTB-5.6.1-win64/bin/otbcli_Rescale"
+  system(paste(o_dir, "-in", input, "-out", output, "-ram 3000", "-outmin 0", "-outmax 255"))
+}
+
+
 HaralickTextureExtration <- function(input, output){
   if(missing(input)){
     stop("Please provide input")}
@@ -19,6 +29,18 @@ HaralickTextureExtration <- function(input, output){
   o_dir <- "E:\\OTB-5.6.1-win64\\OTB-5.6.1-win64\\bin\\otbcli_HaralickTextureExtraction"
   system(paste(o_dir, "-in", input, "-channel", 1, "-texture", "simple", "-parameters.min", "0", "-parameters.max", "255", "-out", output))
 }
+
+# HaralickTextureExtration(input = "D:\\naip_tiffs\\m_2909521_ne_15_1_20141016_20141201.tif")
+
+BandSplit <- function(input, output){
+  if(missing(input)){
+    stop("")}
+  if(missing(output)){
+    stop("")}
+  o_dir <- "E:/OTB-5.6.1-win64/OTB-5.6.1-win64/bin/otbcli_SplitImage"
+  system(paste(o_dir, "-in", input, "-out", output, "-ram 3000"))
+}
+
 
 #make merge bands function to merge R,G,B,NIR, and NDVI
 mergeBands <- function(layer.1, layer.2, out){
@@ -31,6 +53,19 @@ mergeBands <- function(layer.1, layer.2, out){
   o_dir <- "E:\\OTB-5.6.1-win64\\OTB-5.6.1-win64\\bin\\otbcli_ConcatenateImages"
   system(paste(o_dir, "-il", layer.1, layer.2, "-out", out,"-ram 3000", sep = " "))
 }
+
+# BandSelection <- function(input, bands){
+#   if(missing(input)){
+#     stop("")}
+#   if(missing(bands)){
+#     stop("")}
+#   o_dir <- "E:\\OTB-5.6.1-win64\\OTB-5.6.1-win64\\bin\\otbcli_BandMath"
+#   system(paste(o_dir, "-il", input, "-exp", ))
+#   }
+
+
+
+
 
 #Large-Scale Mean Shift (LSMS) segmentation
 #Step 1: mean shift smoothing 
@@ -145,4 +180,29 @@ for (i in 1:length(myfiles)){
 }
 
 
+# my.filenames <- list.files("D:/naip_subset/", pattern=".tif$") 
+myfiles <- list.files("D:/naip_subset/", pattern=".tif$", full.names = TRUE)
+tmpdir <- "D:/sub_process/tmp/"
+vectdir <- "D:/sub_process/vect/"
+
+
+for (i in 1:length(myfiles)){
+  #NDVI
+  NDVI(input = myfiles[[i]], out = paste0(tmpdir, "ndvi", i, ".tif"))
+  #Scale NDVI
+  NDVIScale(input = paste0(tmpdir, "ndvi", i, ".tif"), output = paste0(tmpdir, "ndviscale", i, ".tif"))
+  #Texture on NDVI
+  HaralickTextureExtration(input = paste0(tmpdir, "ndviscale", i, ".tif"), output = paste0(tmpdir, "text", i, ".tif"))
+  #Split Texture
+  # BandSplit(input = paste0(tmpdir, "text", i, ".tif"), output = paste0(tmpdir, "text", i, "band", ".tif"))
+  #Merge desired bands
+  # mergeBands(layer.1 = paste0(tmpdir, "ndvi", i, ".tif"), layer.2 = paste0(tmpdir, "text", i, "band_0.tif"), layer.3 = paste0(tmpdir, "text", i, "band_1.tif"), layer.4 = paste0(tmpdir, "text", i, "band_3.tif"), layer.5 = paste0(tmpdir, "text", i, "band_7.tif"), out = paste0(tmpdir, "merge", i, ".tif"))
+  mergeBands(layer.1 = myfiles[[i]], layer.2 = paste0(tmpdir, "ndviscale", i, ".tif"), out = paste0(tmpdir, "merge", i, ".tif"))
+  #Segmentation
+  meanShiftSmoothing(input = paste0(tmpdir, "merge", i, ".tif"), fout = paste0(tmpdir, "range", i, ".tif"), foutpos = paste0(tmpdir, "spatial", i, ".tif"))
+  LSMSSegmentation(input = paste0(tmpdir, "range", i, ".tif"), inpos = paste0(tmpdir, "spatial", i, ".tif"), out = paste0(tmpdir, "seg", i, ".tif"))
+  # LSMSSmallRegionsMerging(input = paste0(tmpdir, "range", i, ".tif"), inseg = paste0(tmpdir, "seg", i, ".tif"), out = paste0(tmpdir, "smallmerge", i, ".tif", minsize = 3))
+  LSMSVectorization(input = myfiles[[i]], inseg = paste0(tmpdir, "seg", i, ".tif"), outshp = paste0(vectdir, "segfinal", i, ".shp"))
+  
+}
 
