d673d84888be8669bd42e5be2c345ece815b33dd
klandolt4042 <klandolt@vols.utk.edu>
2016-10-30 14:10:26 -0400

    census edit

--- census.R
+++ census.R
@@ -15,6 +15,10 @@ harris <- tx[tx$COUNTYFP == 201,]
 
 geo <- geo.make(state=c("TX"), county= 201, tract = "*")
 bg <- geo.make(state="TX", county= 201, tract = "*", block.group = "*")
+bg1 <- block_groups(state="TX", county=201)
+
+
+bg <- block_groups(state="TX", county=201)
 
 counties <- 201
 # tracts <- tracts(state="TX", county=201)
@@ -22,4 +26,14 @@ counties <- 201
 
 income <- acs.fetch(endyear = 2014, span = 5, geography = bg, table.number = "B19001", col.names="pretty")
 
+income_df <- data.frame(state=income@geography$state,
+                        county=income@geography$county, 
+                        tract=income@geography$tract,
+                        bg=income@geography$blockgroup,
+                        income@estimate[,c(1:17)], 
+                        stringsAsFactors = FALSE)
 
+
+
+
+income_df1 <- data.frame(paste0(str_pad(income@geography$state,2,"left",pad="")))
