389d10dfaa69eb832dde75f9f6206c9128ae3f5c
klandolt4042 <klandolt@vols.utk.edu>
2016-11-19 20:10:39 -0500

    census

--- census.R
+++ census.R
@@ -8,8 +8,11 @@ library(acs)
 library(tigris)
 library(stringr)
 library(rgdal)
+library(rgeos)
 library(SDMTools)
 library(parallel)
+library(foreach)
+library(broom)
 
 #read api.key.install
 # read.lines
@@ -152,60 +155,190 @@ owner_df$merge <- paste(as.character(owner_df$tract), as.character(owner_df$bg),
 join <- geo_join(join, owner_df, "merge", "merge")
 
 
+e <- extent(naip)
+foot <- as(e, "SpatialPolygons")
+crs(foot) <- crs(naip)
 
-writeOGR(join, "E:/Dropbox/Fall2016/GEOG515/data/", "join", driver="ESRI Shapefile")
-sub.join <- readOGR("E:/Dropbox/Fall2016/GEOG515/data", "join_sub")
+id.contain <- as.data.frame(gContains(foot,join, byid=TRUE))
 
-sub.join$can_prop <- NA
-# int.join <- raster::intersect(join, naip)
 
-# canopy.metrics <- list()
+join$contain <- id.contain$`1`
+join <- join[!(join$contain==FALSE),]
 
-# per.cov <- function(img,region){
-#   sub <- crop(img, region)
-#   sub <- mask(sub, region)
-#   sub[sub > 0] <- 2
-#   sub[sub == 0] <- 1
-#   sub[sub == 2] <- 0
-#   return(ClassStat(as.matrix(sub),bkgd=0))
-# }
+drops <- c("STATEFP", "COUNTYFP", "GEOID", "NAMELSAD", "MTFCC", "FUNCSTAT", "ALAND", "AWATER", "INTPTLAT", "INTPTLON", "merge",
+           "state", "county", "state.1", "county.1", "tract.1", "bg.1", "merge.1", "merge.2", "state.2", "county.2", "tract.2", "bg.2", 
+           "merge.3", "state.3", "county.3", "tract.3", "bg.3", "merge.4", "contain")
+join <- join[ , !(names(join) %in% drops)]
 
-for (i in 1:length(sub.join)){
-  clip <- sub.join[i,]
-  sub <- crop(naip, clip)
-  sub <- mask(sub, clip)
-  sub[sub > 0] <- 2
-  sub[sub == 0] <- 1
-  sub[sub == 2] <- 0
-  prop <- ClassStat(as.matrix(sub),bkgd=0)
-  sub.join$can_prop[i] <- prop$prop.landscape
-  cat(i)
+# writeOGR(join, "E:/Dropbox/Fall2016/GEOG515/data/", "join", driver="ESRI Shapefile")
+# sub.join <- readOGR("E:/Dropbox/Fall2016/GEOG515/data", "join_sub")
+# sub.join <- readOGR("/Users/lando/Dropbox/Fall2016/GEOG515/data/", "join_sub")
+
+join$can_prop <- NA
+
+
+join.df <- join@data
+#age.sum = 355456
+age.sum <- colSums(join@data[,48:56])
+#five groups = 71091.2
+#six groups = 88864
+#2000-present
+join@data$y2000_present <- rowSums(join@data[,48:49])
+#1980-2000
+join@data$y1980_2000 <- rowSums(join@data[,50:51])
+#1970-1980
+join@data$y1970_1980 <- join@data[,52]
+#1960-1970
+join@data$y1960_1970 <- join@data[,53]
+#1950-1960
+join@data$y1950_1960 <- join@data[,54]
+#before-1950
+join@data$yPrevious_1950 <- rowSums(join@data[,55:56])
+
+
+
+#inc.sum = 309583
+inc.sum <- colSums(join@data[,6:21])
+#five groups = 61916.6
+#less-20
+join@data$incLess_20 <- rowSums(join@data[,6:8])
+#20-40
+join@data$inc20_40 <- rowSums(join@data[,9:12])
+#40-75
+join@data$inc40_75 <- rowSums(join@data[,13:16])
+#75-125
+join@data$inc75_125 <- rowSums(join@data[,17:18])
+#125-greater
+join@data$inc125_greater <- rowSums(join@data[,19:21])
+
+#val.sum = 138917
+val.sum <- colSums(join@data[,23:46])
+#five groups = 27783.4
+#six groups = 23152.83
+#less - 70
+join@data$valLess_70 <- rowSums(join@data[,23:32])
+#70 - 100
+join@data$val70_100  <- rowSums(join@data[,33:35])
+#100 - 175
+join@data$val100_175 <- rowSums(join@data[,36:38])
+#175 - 300
+join@data$val175_300 <- rowSums(join@data[,39:41])
+#300 - 500
+join@data$val300_500 <- rowSums(join@data[,42:43])
+#500 - greater
+join@data$val500_greater <- rowSums(join@data[,44:46])
+
+
+keeps <- c("TRACTCE", "BLKGRPCE", "tract", "bg", "t_income_house", "t_value_house", "t_year_house", "t.occu", "o.occu", "r.occu",
+           "can_prop", "y2000_present", "y1980_2000", "y1970_1980", "y1960_1970", "y1950_1960", "yPrevious_1950",
+           "incLess_20", "inc20_40", "inc40_75", "inc75_125", "inc125_greater", "valLess_70", "val70_100", "val100_175",
+           "val175_300", "val300_500", "val500_greater")
+join <- join[keeps]
+
+# join.df <- as.data.frame(join)
+
+
+for (i in 1:length(join)){
+  clip <- join[i,]
+  clip1 <- crop(naip, clip)
+  clip2 <- rasterize(clip, clip1, mask=TRUE)
+  clip2[clip2 > 0] <- 2
+  clip2[clip2 == 0] <- 1
+  clip2[clip2 == 2] <- 0
+  prop <- ClassStat(as.matrix(clip2),bkgd=0)
+  join.df$can_prop[i] <- prop$prop.landscape
+  cat(i )
 }
 
-writeOGR(sub.join, dsn = "E:/Dropbox/Fall2016/GEOG515/data/", "final", driver = "ESRI Shapefile")
-final <- readOGR(dsn = "/Users/lando/Dropbox/Fall2016/GEOG515/data/", layer = "final")
+# Write join to shapefile
+write.table(join, file = "/Users/lando/Dropbox/Fall2016/GEOG515/data/join.txt", sep=",")
+writeOGR(join, dsn = "/Users/lando/Dropbox/Fall2016/GEOG515/data/", "final1", driver = "ESRI Shapefile", overwrite_layer = TRUE)
+# writeOGR(join, dsn = "E:/Dropbox/Fall2016/GEOG515/data/", "final", driver = "ESRI Shapefile")
+# final <- readOGR(dsn = "/Users/lando/Dropbox/Fall2016/GEOG515/data/", layer = "final")
 
-final.df <- as.data.frame(final)
+final.join <- (join)
+final.join <- final.join[final.join@data$t_income_house!=0,]
+final.join <- final.join[final.join@data$t_value_house!=0,]
+final.join <- final.join[final.join@data$t_year_house!=0,]
+final.join <- final.join[final.join@data$t.occu!=0,]
 
-income.frac <- final.df[,19:34]/final.df[,18]
-value.frac <- final.df[,41:64]/final.df[,40]
-age.frac <- final.df[,71:79]/final.df[,70]
+final.join@data$income.frac <- final.join@data[,18:22]/final.join@data[,5]
+final.join@data$value.frac <- final.join@data[,23:28]/final.join@data[,6]
+final.join@data$age.frac <- final.join@data[,12:17]/final.join@data[,7]
+final.join@data$owner.frac <- final.join@data[,9:10]/final.join@data[,8]
 
-sub <- cbind(income.frac, value.frac, age.frac)
-sub$canopy <- final.df$can_prop
+smp_size <- floor(0.80 * nrow(final.join))
 
-sub <- sub[complete.cases(sub),]
+set.seed(123)
+train_ind <- sample(seq_len(nrow(final.join)), size= smp_size)
+train_set <- final.join[train_ind,]
+test_set <- final.join[-train_ind,]
 
-#model with just age covariates
-model.age <- glm(canopy~sub$yer2010+sub$y2000_2+sub$y1990_1+sub$y1980_1+sub$y1970_1+sub$y1960_1+sub$y1950_1+sub$y1940_1+sub$yer1940, data=sub, family=quasibinomial(link = "logit"))
+write.table(final.join, file = "/Users/lando/Dropbox/Fall2016/GEOG515/data/join.txt", sep=",")
+writeOGR(final.join, dsn = "/Users/lando/Dropbox/Fall2016/GEOG515/data/", "final1", driver = "ESRI Shapefile", overwrite_layer = TRUE)
 
-# Model with all covariates exclusing 1 from each category
-model.1 <- glm(canopy~.-grtr200 -val1000 -yer1940, data=sub, family = quasibinomial(link = "logit"))
-model.11 <- glm(canopy~.-grtr200 -val1000 -yer1940, data=sub[, -c(1:15)], family = quasibinomial(link = "logit"))
-model.12 <- glm(canopy~.-grtr200 -val1000 -yer1940, data=sub[, -c(17:39)], family = quasibinomial(link = "logit"))
-model.13 <- glm(canopy~.-grtr200 -val1000 -yer1940, data=sub[, -c(41:48)], family = quasibinomial(link = "logit"))
+
+keeps <- c("can_prop", "income.frac", "value.frac", "age.frac", "owner.frac")
+
+train_set <- train_set[keeps]
+train_set@data <- cbind(train_set@data, train_set@data$income.frac, train_set@data$value.frac, train_set@data$age.frac, train_set@data$owner.frac)
+train_set@data$income.frac <- NULL
+train_set@data$value.frac <- NULL
+train_set@data$age.frac <- NULL
+train_set@data$owner.frac <- NULL
+
+test_set@data <- test_set@data[keeps]
+test_set@data <- cbind(test_set@data, test_set@data$income.frac, test_set@data$value.frac, test_set@data$age.frac, test_set@data$owner.frac)
+test_set@data$income.frac <- NULL
+test_set@data$value.frac <- NULL
+test_set@data$age.frac <- NULL
+test_set@data$owner.frac <- NULL
+names(test_set)[names(test_set) == "can_prop"] <- "true_can_prop"
+
+
+# sub <- cbind(income.frac, value.frac, age.frac, owner.frac)
+# sub$canopy <- final.df$can_prop
+
+
+
+# Model with all covariates excluding 1 from each category
+# model.tot <- glm(can_prop~. -income.frac$inc125_greater -value.frac$val500_greater -age.frac$y2000_present -owner.frac$o.occu, data=train_set@data, family = quasibinomial(link = "logit"))
+model.tot <- glm(can_prop~. -inc125_greater -val500_greater -y2000_present -o.occu, data=train_set@data, family = quasibinomial(link = "logit"))
+
+test_set$pred_can_prop <- predict(model.tot, test_set@data, type="response")
+test_set$error <- test_set$true_can_prop - test_set$pred_can_prop
+
+#Plotting shapefile
+train_set@data$id = rownames(train_set@data)
+train_set.fort = tidy(train_set, region = "id")
+train_set.df = join(train_set.fort, train_set@data, by="id")
+
+ggplot(train_set.df) + aes(long, lat, group=group, fill=can_prop) + geom_polygon() + coord_equal()
+
+
+test_set@data$id = rownames(test_set@data)
+test_set.fort = tidy(test_set, region = "id")
+test_set.df = join(test_set.fort, test_set@data, by="id")
+ggplot(test_set.df) + aes(long, lat, group=group, fill=error) + geom_polygon() + coord_equal()
+
+
+
+
+# Only Single type of category models
+model.year <- glm(canopy~. -y2000_present, data=sub[, -c(1:11, 18:19)], family = quasibinomial(link = "logit"))
+model.value <- glm(canopy~. -val500_greater, data=sub[, -c(1:5, 12:19)], family = quasibinomial(link = "logit"))
+model.owner <- glm(canopy~. -o.occu, data=sub[, -c(1:17)], family = quasibinomial(link = "logit"))
+model.inc <- glm(canopy~. -inc125_greater, data=sub[, -c(6:19)], family = quasibinomial(link = "logit"))
+
+# Subracting single type of variable models
+model.sub.year <- glm(canopy~. -inc125_greater -val500_greater -o.occu, data=sub[, -c(12:16)], family = quasibinomial(link = "logit"))
+model.sub.value <- glm(canopy~. -inc125_greater -y2000_present -o.occu, data=sub[, -c(6:11)], family = quasibinomial(link = "logit"))
+model.sub.owner <- glm(canopy~. -inc125_greater -val500_greater -y2000_present, data=sub[, -c(18:19)], family = quasibinomial(link = "logit"))
+model.sub.inc <- glm(canopy~. -val500_greater -y2000_present -o.occu, data=sub[, -c(1:5)], family = quasibinomial(link = "logit"))
+
+
 anova(model.1, model.11, test='Chisq')
 anova(model.1, model.12, test='Chisq')
 anova(model.1, model.13, test='Chisq')
 
-model.2 <- glm(canopy~.-grtr200 -val1000 -yer1940, data=sub, family = quasibinomial)
+
